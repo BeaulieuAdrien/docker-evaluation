80a812d73c0c2058d29b40c9f9fb06d6530b3eb7
Adrien Beaulieu <contact@adrienbeaulieu.fr>
2023-11-07 19:55:45 +0100

    wip app.js

--- docker-api/app/app.js
+++ docker-api/app/app.js
@@ -3,3 +3,24 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
+
+const app = express();
+
+app.use([
+    helmet(),
+    cors(),
+    morgan('tiny'),
+    hello()
+]);
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+// Load controllers
+// TO DO
+
+// Export app
+module.exports = app;
+
